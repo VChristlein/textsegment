95227221eaa32c98a70ebd4f0885494f0b882bee
Hendrik Schröter <hendrik.m.schroeter@fau.de>
2017-12-14 09:28:44 +0100

    Refactor input and always do a crf training after normal training

--- dataset/dibco.py
+++ dataset/dibco.py
@@ -10,19 +10,32 @@ from utils.data import maybe_download, dict_to_example, get_label_map_dict
 from utils.image_processing import preprocess, scale, inv_preprocess, \
   random_rotate, get_gt_img, map_ground_truth
 
-DATA_URL = 'https://www.dropbox.com/s/62pps8pi5jfqzxg/dibco.zip?dl=1'
+
+def get_dibco_meta_data():
+  return {
+    'url': 'https://www.dropbox.com/s/62pps8pi5jfqzxg/dibco.zip?dl=1',
+    'img_mean': [196.48484802, 188.59724426, 170.53767395],
+    'num_classes': 2,
+    'num_img_train': 68,
+    'num_img_val': 18,
+    'img_channels': 3,
+    'gt_channels': 1,
+    'default_img_height': 250,
+    'default_img_width': 250,
+  }
+
 DEFAULT_DATA_DIR = '/tmp/dibco'
-DEFAULT_RECORD_DIR = '/tmp/dibco'
 DATA_EXTRACTED_DIR = 'dibco'
-IMG_MEAN = [196.48484802, 188.59724426, 170.53767395]
 
 
 def prepare_dibco(data_dir=DEFAULT_DATA_DIR,
-                  out_dir=DEFAULT_RECORD_DIR,
+                  out_dir=None,
                   force=False):
   """ Downloads and extracts dibco dataset and its annotation data. """
   if not os.path.exists(data_dir):
     os.makedirs(data_dir)
+  if out_dir is None:
+    out_dir = data_dir
   if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
@@ -32,7 +45,7 @@ def prepare_dibco(data_dir=DEFAULT_DATA_DIR,
   if not (os.path.exists(os.path.join(data_dir, train_record)) and
             os.path.exists(os.path.join(data_dir, val_record))) \
       or force:
-    maybe_download(DATA_URL, data_dir, force=force)
+    maybe_download(get_dibco_meta_data()['url'], data_dir, force=force)
 
     data_dir = os.path.join(data_dir, DATA_EXTRACTED_DIR)
 
@@ -51,6 +64,8 @@ def prepare_dibco(data_dir=DEFAULT_DATA_DIR,
       val_writer.write(example.SerializeToString())
       val_writer.close()
 
+  return get_dibco_meta_data()
+
 
 def get_dibco_palette():
   return tf.constant(
@@ -64,22 +79,29 @@ def get_dibco_palette():
 def dibco_input_fn(is_training,
                    num_epochs=1,
                    batch_size=1,
+                   img_size=250,
                    img_scale_factor=1,
                    label_size=None,
                    buffer_size=200,
-                   record_dir=DEFAULT_RECORD_DIR,
                    data_dir=DEFAULT_DATA_DIR):
-  prepare_dibco(data_dir, record_dir)
-
-  height, width, channels_img, channels_gt = (250, 250, 3, 1)
+  if isinstance(img_size, tuple):
+    height, width = img_size
+  else:
+    height = width = img_size
+  channels_img, channels_gt = (3, 1)
   out_height = int(img_scale_factor * height)
   out_width = int(img_scale_factor * width)
   if label_size is None:
     label_size = (out_height, out_width)
 
   file_names = os.path.join(
-    record_dir, 'train.record' if is_training else 'val.record')
-  data_set = tf.contrib.data.TFRecordDataset(file_names)
+    data_dir, 'train.record' if is_training else 'val.record')
+  if not os.path.exists(file_names):
+    raise ValueError('TFRecord not found: {}.'.format(file_names) + \
+                     'Did you download it `using prepare_dibco()`?')
+  data_set = tf.data.TFRecordDataset(file_names)
+
+  mean = get_dibco_meta_data()['img_mean']
 
   def dataset_parser(record):
     keys_to_features = {
@@ -94,7 +116,7 @@ def dibco_input_fn(is_training,
     gt = tf.image.decode_png(
       parsed['ground_truth/encoded'], channels=channels_gt)
 
-    image, gt = preprocess(image, gt, height, width, IMG_MEAN, is_training)
+    image, gt = preprocess(image, gt, height, width, mean, is_training)
     image = scale(image, scale_factor=img_scale_factor)
     gt = scale(gt, out_size=label_size)
     gt = map_ground_truth(gt, get_dibco_palette())
@@ -118,7 +140,8 @@ def dibco_input_fn(is_training,
   else:
     mode_str = 'eval'
 
-  tf.summary.image(mode_str + '/original', inv_preprocess(images, IMG_MEAN),
+  tf.summary.image(mode_str + '/original',
+                   inv_preprocess(images, mean),
                    max_outputs=6)
   tf.summary.image(mode_str + '/ground_truth',
                    get_gt_img(tf.squeeze(labels, axis=3), get_dibco_palette()),
--- train.py
+++ train.py
@@ -11,9 +11,10 @@ from models.unet import unet_model_fn_gen as model_fn_generator
 # from dataset.pascal_voc import pascal_voc_input_fn as input_fn, \
 #                                get_gt_img as gt_fn, \
 #                                get_pascal_palette as get_palette
-from dataset.dibco import dibco_input_fn as input_fn, \
-                          get_gt_img as gt_fn, \
-                          get_dibco_palette as get_palette
+from dataset.dibco import prepare_dibco as prepare_dataset, \
+  dibco_input_fn as input_fn, \
+  get_gt_img as gt_fn, \
+  get_dibco_palette as get_palette
 
 parser = argparse.ArgumentParser()
 
@@ -39,38 +40,39 @@ parser.add_argument('--batch_size', type=int, default=1,
 parser.add_argument('--buffer_size', type=int, default=0,
                     help='The number of images to buffer for training.')
 
+parser.add_argument('--img_patch_size', type=int, default=0,
+                    help='Input image size using random crop and pad. ' + \
+                         'If not provided use a dataset specific default.')
+
 parser.add_argument('--scale_factor', type=float, default=1,
                     help='Input image scale factor between (0, 1].')
-
-parser.add_argument('--crf_training', type=bool, default=False,
-                    help='After normal training train a downstream crf')
 
 FLAGS = parser.parse_args()
 
 if FLAGS.buffer_size == 0:
   FLAGS.buffer_size = FLAGS.batch_size * 100
 
-_NUM_CLASSES = 2
-
-_HEIGHT = int(250 * FLAGS.scale_factor)
-_WIDTH = int(250 * FLAGS.scale_factor)
-_DEPTH = 3
-_NUM_IMAGES = {
-  'train': 68,
-  'validation': 18,
-}
-
 # Scale the learning rate linearly with the batch size. When the batch size is
 _INITIAL_LEARNING_RATE = 0.1 * FLAGS.batch_size / 64
 _NUM_EPOCHS_PER_DECAY = 250
 _MOMENTUM = 0.9
 
-_WEIGHT_DECAY = 2e-5 / (2 * _NUM_IMAGES['train'])
-
-_BATCHES_PER_EPOCH = _NUM_IMAGES['train'] // FLAGS.batch_size
-
 
 def main(unused_argv):
+  # Prepare the dataset
+  meta_data = prepare_dataset(data_dir=FLAGS.data_dir)
+  if FLAGS.img_patch_size > 0:
+    height = width = FLAGS.img_patch_size
+  else:
+    height = meta_data['default_img_height']
+    width = meta_data['default_img_width']
+  height = int(height * FLAGS.scale_factor)
+  width = int(width * FLAGS.scale_factor)
+  depth = meta_data['img_channels']
+
+  weight_decay = 2e-5 / (2 * meta_data['num_img_train'])
+  batches_per_epoch = meta_data['num_img_train'] // FLAGS.batch_size
+
   # Using the Winograd non-fused algorithms provides a small performance boost.
   os.environ['TF_ENABLE_WINOGRAD_NONFUSED'] = '1'
 
@@ -83,53 +85,52 @@ def main(unused_argv):
     num_epochs=FLAGS.epochs_per_eval,
     batch_size=FLAGS.batch_size,
     buffer_size=FLAGS.buffer_size,
-    record_dir=FLAGS.data_dir,
     data_dir=FLAGS.data_dir)
 
   input_val = lambda: input_fn(
     is_training=False,
     img_scale_factor=FLAGS.scale_factor,
-    record_dir=FLAGS.data_dir,
     data_dir=FLAGS.data_dir)
 
-  decay_steps = int(_BATCHES_PER_EPOCH * _NUM_EPOCHS_PER_DECAY)
-  model_fn = model_fn_generator(
-    unet_depth=FLAGS.unet_depth,
-    num_classes=_NUM_CLASSES,
-    # ignore_last_class=True,
-    get_gt_fn=lambda predictions: gt_fn(predictions, get_palette()),
-    input_shape=[_HEIGHT, _WIDTH, _DEPTH],
-    initial_learning_rate=_INITIAL_LEARNING_RATE,
-    learning_rate_decay_every_n_steps=decay_steps,
-    momentum=_MOMENTUM,
-    weight_decay=_WEIGHT_DECAY,
-    crf_post_processing=FLAGS.crf_training,
-    save_dir=FLAGS.model_dir)
+  decay_steps = int(batches_per_epoch * _NUM_EPOCHS_PER_DECAY)
 
-  classifier = tf.estimator.Estimator(
-    model_fn=model_fn,
-    model_dir=FLAGS.model_dir,
-    config=run_config)
+  for use_crf in range(2):
+    model_fn = model_fn_generator(
+      unet_depth=FLAGS.unet_depth,
+      num_classes=meta_data['num_classes'],
+      get_gt_fn=lambda predictions: gt_fn(predictions, get_palette()),
+      input_shape=[height, width, depth],
+      initial_learning_rate=_INITIAL_LEARNING_RATE,
+      learning_rate_decay_every_n_steps=decay_steps,
+      momentum=_MOMENTUM,
+      weight_decay=weight_decay,
+      crf_post_processing=use_crf,
+      save_dir=FLAGS.model_dir)
 
-  for _ in range(FLAGS.train_epochs // FLAGS.epochs_per_eval):
-    tensors_to_log = {
-      'learning_rate': 'learning_rate',
-      'cross_entropy': 'cross_entropy',
-      'train_accuracy': 'train_accuracy'
-    }
+    classifier = tf.estimator.Estimator(
+      model_fn=model_fn,
+      model_dir=FLAGS.model_dir,
+      config=run_config)
 
-    logging_hook = tf.train.LoggingTensorHook(
-      tensors=tensors_to_log, every_n_iter=1000)
+    for _ in range(FLAGS.train_epochs // FLAGS.epochs_per_eval):
+      tensors_to_log = {
+        'learning_rate': 'learning_rate',
+        'cross_entropy': 'cross_entropy',
+        'train_accuracy': 'train_accuracy'
+      }
 
-    classifier.train(
-      input_fn=input_train,
-      hooks=[logging_hook])
+      logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=1000)
 
-    # Evaluate the model and print results
-    print('Evaluating model ...')
-    eval_results = classifier.evaluate(
-      input_fn=input_val)
-    print(eval_results)
+      classifier.train(
+        input_fn=input_train,
+        hooks=[logging_hook])
+
+      # Evaluate the model and print results
+      print('Evaluating model ...')
+      eval_results = classifier.evaluate(
+        input_fn=input_val)
+      print(eval_results)
 
 
 if __name__ == '__main__':
